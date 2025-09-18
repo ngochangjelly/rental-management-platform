e62e62508d72fe4330729a904ea84b689d1e4cc5
ngochangjelly <ngochang29397@gmail.com>
2025-09-18 23:31:15 +0800

    fix bug icon

--- public/sw.js
+++ public/sw.js
@@ -10,7 +10,7 @@ const STATIC_ASSETS = [
 
 const CDN_ASSETS = [
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
 ];
 
